6e4cee4605ea5b7d7597cf7c44ab500653e1ff6c
Qodesmith <theqodesmith@gmail.com>
2024-06-29 08:12:55 -0400

    TW 4 is too much hassle. Using the latest v3

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1>Beats Playlist!</h1>
+    <h1 className="text-3xl font-bold underline">H1 - Beats Playlist!</h1>
+    <h2>H2 - Beats Playlist!</h2>
+    <h3>H3 - Beats Playlist!</h3>
+    <button className="btn">Button</button>
   </React.StrictMode>
 )
