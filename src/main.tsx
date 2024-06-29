bc59457b4090da1551cc66c27fe179a6941de554
Qodesmith <theqodesmith@gmail.com>
2024-06-29 15:10:34 -0400

    Added color themes from uicolors.app

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,6 @@ import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1 className="text-3xl font-bold underline">H1 - Beats Playlist!</h1>
-    <h2>H2 - Beats Playlist!</h2>
-    <h3>H3 - Beats Playlist!</h3>
-    <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-      Button
-    </button>
+    <h1>Beats Playlist!</h1>
   </React.StrictMode>
 )
