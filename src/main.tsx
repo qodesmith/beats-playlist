0234c9f485ac6cff1f72b0004df98e2805980deb
Qodesmith <theqodesmith@gmail.com>
2024-06-29 08:31:43 -0400

    Adding project settings for vscode

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <h1 className="text-3xl font-bold underline">H1 - Beats Playlist!</h1>
     <h2>H2 - Beats Playlist!</h2>
     <h3>H3 - Beats Playlist!</h3>
-    <button className="btn">Button</button>
+    <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+      Button
+    </button>
   </React.StrictMode>
 )
