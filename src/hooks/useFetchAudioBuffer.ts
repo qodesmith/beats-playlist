81b52549d7fb4f01f10cbabd8885f3afb724efd9
Qodesmith <theqodesmith@gmail.com>
2024-07-03 13:19:16 -0400

    Cache data longer

--- src/hooks/useFetchAudioBuffer.ts
+++ src/hooks/useFetchAudioBuffer.ts
@@ -8,6 +8,8 @@ export function useFetchAudioBuffer(fileName: string) {
         .then(res => res.arrayBuffer())
         .then(arrayBuffer => new AudioContext().decodeAudioData(arrayBuffer))
     },
+    staleTime: Infinity,
+    gcTime: 1000 * 60 * 60 * 24, // 1 day
   })
 
   return {audioBuffer, ...rest}
