72f11c855a2a26d4690c23f5fdd5cdfdb98abac5
Qodesmith <theqodesmith@gmail.com>
2024-07-03 12:19:14 -0400

    Basic dark-mode styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import {Visualizer} from './components/Visualizer/Visualizer'
 import {QueryClientProvider} from '@tanstack/react-query'
 import {queryClient} from './appFoundation'
 
+// Style the app in dark mode.
+document.body.classList.add('dark:bg-slate-800', 'dark:text-white')
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
