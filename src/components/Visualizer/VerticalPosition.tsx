f820e2ca12ec759355ca13493350e55cdcdf1f9e
Qodesmith <theqodesmith@gmail.com>
2024-07-03 22:51:45 -0400

    Vertical cursor only shows when mousing over waveform

--- src/components/Visualizer/VerticalPosition.tsx
+++ src/components/Visualizer/VerticalPosition.tsx
@@ -1,18 +1,18 @@
 import {useCallback, useRef, useState} from 'react'
 import {WaveformStyle} from './WaveForm'
 
-const CURSOR_WIDTH = 2
+const CURSOR_WIDTH = 1
 
 export function VerticalPosition({style}: {style: WaveformStyle | undefined}) {
+  const [shouldShow, setShouldShow] = useState<boolean>(false)
   const [left, setLeft] = useState<number>(0)
   const containerRef = useRef<HTMLDivElement>(null)
   const cursorHeight = style === 'reflection' ? 'h-4/5' : 'h-full'
   const handleMouseMove = useCallback(
     (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-      const offsetLeft = containerRef.current?.getBoundingClientRect().left ?? 0
-      const maxValue =
-        (containerRef.current?.getBoundingClientRect().width ?? Infinity) -
-        CURSOR_WIDTH
+      const domRect = containerRef.current?.getBoundingClientRect()
+      const offsetLeft = domRect?.left ?? 0
+      const maxValue = (domRect?.width ?? Infinity) - CURSOR_WIDTH
       const calculatedValue = e.clientX - offsetLeft
       const newValue = calculatedValue < 0 ? 0 : calculatedValue
 
@@ -20,17 +20,27 @@ export function VerticalPosition({style}: {style: WaveformStyle | undefined}) {
     },
     []
   )
+  const handleMouseEnter = useCallback(() => {
+    setShouldShow(true)
+  }, [])
+  const handleMouseLeave = useCallback(() => {
+    setShouldShow(false)
+  }, [])
 
   return (
     <div
       className="absolute size-full opacity-75"
       onMouseMove={handleMouseMove}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       ref={containerRef}
     >
-      <div
-        className={`absolute ${cursorHeight} w-0.5 bg-red-600`}
-        style={{left}}
-      />
+      {shouldShow && (
+        <div
+          className={`absolute ${cursorHeight} w-px bg-red-600`}
+          style={{left}}
+        />
+      )}
     </div>
   )
 }
