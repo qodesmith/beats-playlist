52d6acb6378f6d6087f3cb0f13976dd98ffdae7d
Qodesmith <theqodesmith@gmail.com>
2024-07-05 15:47:14 -0400

    Exclude public dir from vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({command}) => ({
+  publicDir: command === 'build' ? false : 'public',
   plugins: [react()],
   server: {
     open: true,
   },
-})
+}))
