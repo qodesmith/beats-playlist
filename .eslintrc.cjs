6354d30df8498ed26a982d07ec52782272be7b37
Qodesmith <theqodesmith@gmail.com>
2024-07-05 15:40:16 -0400

    Eslint / prettier sorting setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,44 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {browser: true, es2020: true},
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/typescript',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint', 'import'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      {allowConstantExport: true},
+    ],
+    'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: [
+          'type',
+          'builtin',
+          'external',
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+          'object',
+        ],
+        alphabetize: {order: 'asc', caseInsensitive: true},
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+        disallowTypeAnnotations: false,
+      },
     ],
   },
 }
