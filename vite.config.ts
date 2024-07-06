e729305a2ff115a232326f84549ca4572230d109
Qodesmith <theqodesmith@gmail.com>
2024-07-06 07:56:04 -0400

    Proxying api endpoints in the Vite server

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 import {defineConfig} from 'vite'
+
+dotenv.config()
+
+const {UNRAID_API} = process.env
 
 // https://vitejs.dev/config/
 export default defineConfig(({command}) => ({
@@ -7,5 +12,14 @@ export default defineConfig(({command}) => ({
   plugins: [react()],
   server: {
     open: true,
+    proxy: UNRAID_API
+      ? ['/beats', '/metadata', '/thumbnails'].reduce<Record<string, string>>(
+          (acc, endpoint) => {
+            acc[endpoint] = UNRAID_API
+            return acc
+          },
+          {}
+        )
+      : undefined,
   },
 }))
