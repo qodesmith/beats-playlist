9adfc986da8d6e72cfa2fe8c396f3d6c7a379dc8
Qodesmith <theqodesmith@gmail.com>
2024-07-10 10:14:25 -0400

    Manually handling copying files from /public => /dist upon build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import type {PluginOption} from 'vite'
 
+import fs from 'node:fs'
+
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 import {defineConfig} from 'vite'
@@ -12,7 +14,7 @@ const UNRAID_API = NO_UNRAID === 'true' ? undefined : process.env.UNRAID_API
 // https://vitejs.dev/config/
 export default defineConfig(({command}) => ({
   publicDir: command === 'build' ? false : 'public',
-  plugins: [react(), rewriteUnraidApiPaths()],
+  plugins: [react(), rewriteUnraidApiPaths(), copyPublicAssetsAfterBuild()],
   clearScreen: false,
   server: {
     open: true,
@@ -73,6 +75,30 @@ function rewriteUnraidApiPaths(): PluginOption {
         }
 
         next()
+      })
+    },
+  }
+}
+
+/**
+ * Since we're mimicing the unraid server with the `/public/beats` folder, we
+ * don't want that copied over into the build. This plugin copies all files from
+ * the `/public` folder root and ignores any sub-directories.
+ */
+function copyPublicAssetsAfterBuild(): PluginOption {
+  return {
+    name: 'copyPublicAssetsAfterBuild',
+    closeBundle() {
+      const items = fs.readdirSync('./public')
+      const files = items.filter(item =>
+        fs.statSync(`./public/${item}`).isFile()
+      )
+
+      files.forEach(file => {
+        const publicPath = `./public/${file}`
+        const distPath = `./dist/${file}`
+
+        fs.copyFileSync(publicPath, distPath)
       })
     },
   }
