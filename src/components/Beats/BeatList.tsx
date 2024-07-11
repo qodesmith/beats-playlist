a94c9df5f0023e48790fc7364a14faa5785c595c
Qodesmith <theqodesmith@gmail.com>
2024-07-11 09:27:57 -0400

    Thumbnail image handling

--- src/components/Beats/BeatList.tsx
+++ src/components/Beats/BeatList.tsx
@@ -1,4 +1,5 @@
 import {useSetAtom} from 'jotai'
+import {useCallback, useState} from 'react'
 
 import {selectedBeatFileNameAtom} from './state'
 import {useFetchMetadata} from '../../hooks/useFetchMetadata'
@@ -11,18 +12,44 @@ export function BeatList() {
 
   return (
     <ul>
-      {metadata.map(({id, title, audioFileExtension}) => {
-        return (
-          <li
-            key={id}
-            onClick={() => {
-              setBeatFileName(`${title} [${id}].${audioFileExtension}`)
-            }}
-          >
-            {title}
-          </li>
-        )
-      })}
+      {metadata
+        .filter(item => item.isUnavailable)
+        .map(({id, title, audioFileExtension, channelName}) => {
+          return (
+            <li
+              key={id}
+              className="mb-4 flex items-center gap-2"
+              onClick={() => {
+                setBeatFileName(`${title} [${id}].${audioFileExtension}`)
+              }}
+            >
+              <ThumbnailImage src={`/thumbnails/${id}[small]`} />
+              <div className="flex flex-col">
+                <div>{title}</div>
+                <div className="text-sm opacity-50">
+                  {channelName || <>&mdash;</>}
+                </div>
+              </div>
+            </li>
+          )
+        })}
     </ul>
+  )
+}
+
+function ThumbnailImage({src}: {src: string}) {
+  const [showDefault, setShowDefault] = useState(false)
+  const handleImgError = useCallback(() => {
+    setShowDefault(true)
+  }, [])
+
+  return showDefault ? (
+    <div className="h-[40px] w-[40px] rounded bg-gray-500" />
+  ) : (
+    <img
+      src={src}
+      className="h-[40px] w-[40px] rounded"
+      onError={handleImgError}
+    />
   )
 }
