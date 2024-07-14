6a5a0690246eebd84fe1d624a08af32694ac10d5
Qodesmith <theqodesmith@gmail.com>
2024-07-14 19:36:57 -0400

    Using horizontal space for the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,14 @@ import {metadataStatsSelector} from './Beats/state'
 
 export function Header() {
   return (
-    <header className="pb-4">
+    <header className="flex items-end gap-8 pb-4">
       <h1 className="text-5xl font-bold">Beats Playlist</h1>
-      <p className="italic opacity-50">Dope beats from YouTube</p>
-      <Suspense fallback="Loading stats...">
-        <HeaderMetadataStats />
-      </Suspense>
+      <div className="text-sm">
+        <p className="italic opacity-50">Dope beats from YouTube</p>
+        <Suspense fallback="Loading stats...">
+          <HeaderMetadataStats />
+        </Suspense>
+      </div>
     </header>
   )
 }
