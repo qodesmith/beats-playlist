31c9dbf88da602b343047837027456dc81e463b9
Qodesmith <theqodesmith@gmail.com>
2024-07-14 15:52:42 -0400

    Improved thumbnail handling

--- src/components/Beats/BeatList.tsx
+++ src/components/Beats/BeatList.tsx
@@ -1,50 +1,60 @@
+import {secondsToDuration} from '@qodestack/utils'
 import {useAtomValue, useSetAtom} from 'jotai'
-import {useCallback, useState} from 'react'
+import {useCallback} from 'react'
 
-import {metadataAtom, selectedBeatFileNameAtom} from './state'
+import {metadataAtom, selectedBeatIdAtom} from './state'
 
 export function BeatList() {
   const metadata = useAtomValue(metadataAtom)
-  const setBeatFileName = useSetAtom(selectedBeatFileNameAtom)
+  const setBeatId = useSetAtom(selectedBeatIdAtom)
+  const handleImageError = useCallback(
+    (e: React.SyntheticEvent<HTMLDivElement, Event>) => {
+      const img = e.target as HTMLImageElement
+      img.style.display = 'none'
+    },
+    []
+  )
 
   return (
-    <ul>
-      {metadata.map(({id, title, audioFileExtension, channelName}) => {
-        return (
-          <li
-            key={id}
-            className="mb-4 flex items-center gap-2"
-            onClick={() => {
-              setBeatFileName(`${title} [${id}].${audioFileExtension}`)
-            }}
-          >
-            <ThumbnailImage src={`/thumbnails/${id}[small]`} />
-            <div className="flex flex-col">
-              <div>{title}</div>
-              <div className="text-sm opacity-50">
-                {channelName || <>&mdash;</>}
+    <>
+      {metadata.map(
+        ({id, title, channelName, dateAddedToPlaylist, durationInSeconds}) => {
+          const dateAdded = new Date(dateAddedToPlaylist).toLocaleDateString()
+          const loadWaveform = () => {
+            setBeatId(id)
+          }
+
+          return (
+            <div
+              key={id}
+              className="grid grid-cols-[40px_1fr_10ch_5ch]"
+              onClick={loadWaveform}
+            >
+              {/* THUMBNAIL */}
+              <div
+                className="h-[40px] w-[40px] overflow-hidden rounded bg-gray-500"
+                onError={handleImageError}
+              >
+                <img src={`/thumbnails/${id}[small]`} />
               </div>
+
+              {/* TITLE / ARTIST */}
+              <div className="flex flex-col">
+                <div>{title}</div>
+                <div className="text-sm opacity-50">
+                  {channelName || <>&mdash;</>}
+                </div>
+              </div>
+
+              {/* DATE ADDED */}
+              <div>{dateAdded}</div>
+
+              {/* DURATION */}
+              <div>{secondsToDuration(durationInSeconds)}</div>
             </div>
-          </li>
-        )
-      })}
-    </ul>
-  )
-}
-
-function ThumbnailImage({src}: {src: string}) {
-  const [showDefault, setShowDefault] = useState(false)
-  const handleImgError = useCallback(() => {
-    setShowDefault(true)
-  }, [])
-
-  return showDefault ? (
-    <div className="h-[40px] w-[40px] rounded bg-gray-500" />
-  ) : (
-    <img
-      src={src}
-      className="h-[40px] w-[40px] rounded"
-      onError={handleImgError}
-    />
+          )
+        }
+      )}
+    </>
   )
 }
