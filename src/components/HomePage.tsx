2aa0af104dedb8d3bf64e76a7d7b4ef14ad4df86
Qodesmith <theqodesmith@gmail.com>
2024-07-20 18:04:25 -0400

    Framer motion; hompage fading from loader to content

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,3 +1,4 @@
+import {AnimatePresence, motion} from 'framer-motion'
 import {useAtomValue} from 'jotai'
 
 import {AudioLoader} from './AudioLoader'
@@ -6,22 +7,40 @@ import {Footer} from './Footer'
 import {Header} from './Header'
 import {isAppInitializedAtom} from '../globalState'
 
+const duration = 0.5
+const opacity0 = {opacity: 0}
+const opacity1 = {opacity: 1}
+
 export function HomePage() {
   const isAppInitialized = useAtomValue(isAppInitializedAtom)
 
   return (
-    <div className="flex h-full flex-col">
+    <AnimatePresence mode="popLayout">
       {isAppInitialized ? (
-        <>
+        <motion.div
+          key={1}
+          id="home-page"
+          className="flex h-full flex-col"
+          initial={opacity0}
+          animate={opacity1}
+          transition={{duration}}
+        >
           <Header />
           <BeatList />
           <Footer />
-        </>
+        </motion.div>
       ) : (
-        <div className="grid h-full w-full place-content-center">
+        <motion.div
+          key={2}
+          className="grid h-full w-full place-content-center"
+          initial={opacity1}
+          animate={opacity1}
+          exit={opacity0}
+          transition={{duration}}
+        >
           <AudioLoader size={100} />
-        </div>
+        </motion.div>
       )}
-    </div>
+    </AnimatePresence>
   )
 }
