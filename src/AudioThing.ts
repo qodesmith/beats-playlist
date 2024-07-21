dade8ac7cb75bc560835f1374feef088ea3f72ad
Qodesmith <theqodesmith@gmail.com>
2024-07-21 08:51:03 -0400

    An audio class and hook to handle the web audio api stuff

---
+++ src/AudioThing.ts
@@ -0,0 +1,87 @@
+import type {audioDataAtomFamily} from './globalState'
+import type {ExtractAtomValue} from 'jotai'
+
+import {audioThingAtom, currentAudioStateAtom} from './globalState'
+import {store} from './store'
+import {createAudioBufferSource} from './utils'
+
+type AudioThingInput = NonNullable<
+  Awaited<ExtractAtomValue<ReturnType<typeof audioDataAtomFamily>>>
+>
+
+export class AudioThing {
+  #audioContext: AudioContext
+  #audioBuffer: AudioBuffer
+  #rms: number
+  #audioSource: AudioBufferSourceNode
+  #startTime: number
+  #pausedTime: number
+
+  constructor(data: AudioThingInput) {
+    const audioContext = new AudioContext()
+    const {audioBuffer, rms} = data
+
+    this.#audioContext = audioContext
+    this.#audioBuffer = audioBuffer
+    this.#rms = rms
+    this.#audioSource = createAudioBufferSource({audioContext, audioBuffer})
+    this.#startTime = 0
+    this.#pausedTime = 0
+
+    store.set(audioThingAtom, this)
+  }
+
+  private createAudioSource() {
+    this.#audioSource = createAudioBufferSource({
+      audioContext: this.#audioContext,
+      audioBuffer: this.#audioBuffer,
+    })
+  }
+
+  togglePlay() {
+    const playState = store.get(currentAudioStateAtom)
+
+    if (playState === 'playing') {
+      store.set(currentAudioStateAtom, 'stopped')
+      this.#pausedTime += this.#audioContext.currentTime - this.#startTime
+      this.#audioSource.stop()
+      this.createAudioSource()
+    } else {
+      store.set(currentAudioStateAtom, 'playing')
+      this.#startTime = this.#audioContext.currentTime
+      this.#audioSource.start(0, this.#pausedTime)
+    }
+  }
+
+  setPlayPosition(position: number) {
+    // Calculate the new start time in the buffer
+    const newTime = this.#audioBuffer.duration * position
+
+    // Stop the current playback
+    this.#audioSource.stop()
+    this.createAudioSource()
+
+    // Update the pausedTime to the new start time
+    this.#pausedTime = newTime
+
+    // Check if the audio is currently playing
+    const playState = store.get(currentAudioStateAtom)
+
+    if (playState === 'playing') {
+      // If playing, start the playback from the new time
+      this.#startTime = this.#audioContext.currentTime
+      this.#audioSource.start(0, this.#pausedTime)
+    }
+  }
+
+  remove() {
+    this.#audioContext.close().then(() => {
+      store.set(currentAudioStateAtom, 'stopped')
+      store.set(audioThingAtom, undefined)
+    })
+  }
+
+  setRepeat(value: boolean) {
+    this.#audioSource.loop = value
+  }
+}
