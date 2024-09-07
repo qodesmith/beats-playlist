6e934af9d4047e1a465af7b22387b24c4674fffb
Qodesmith <theqodesmith@gmail.com>
2024-09-07 07:23:59 -0400

    Mongo will handle connecting

--- server/mongo.ts
+++ server/mongo.ts
@@ -3,14 +3,13 @@ import {MongoClient} from 'mongodb'
 const {MONGO_USER, MONGO_PASSWORD, MONGO_HOST, MONGO_PORT} = Bun.env
 const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}`
 
-let mongoClient: MongoClient
+/**
+ * https://www.mongodb.com/docs/drivers/node/current/fundamentals/connection/connect/#std-label-node-connect-to-mongodb
+ *
+ * No need to call `await mongoClient.connect()` as the Node.js driver
+ * automatically calls the MongoClient.connect() method when using the client to
+ * perform CRUD operations.
+ */
+const mongoClient = new MongoClient(mongoUrl, {family: 4})
 
-export async function genMongoClient(): Promise<MongoClient> {
-  if (mongoClient) return mongoClient
-
-  const client = new MongoClient(mongoUrl, {family: 4})
-  await client.connect()
-  mongoClient = client
-
-  return client
-}
+export const db = mongoClient.db('beats-playlist')
