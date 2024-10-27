7922895b807d3fda8ea096a211935d95c6e9cb1a
Qodesmith <theqodesmith@gmail.com>
2024-10-27 10:16:00 -0400

    Fixed styles with CloseButton

--- src/components/CloseButton.tsx
+++ src/components/CloseButton.tsx
@@ -1,11 +1,15 @@
 import type {ComponentProps} from 'react'
 
+import {useMemo} from 'react'
+
 export function CloseButton({
   size = 30,
   fill = 'white',
   onClick,
   className,
 }: {size?: number; fill?: string} & ComponentProps<'button'>) {
+  const stroke = useMemo(() => ({stroke: fill}), [fill])
+
   return (
     <button onClick={onClick} className={className}>
       <svg
@@ -20,14 +24,16 @@ export function CloseButton({
           width="30"
           height="4"
           rx="2"
-          className="origin-center rotate-45 stroke-black stroke-0 transition-all duration-200 md:group-hover:stroke-2"
+          style={stroke}
+          className="origin-center rotate-45 stroke-0 transition-all duration-200 md:group-hover:stroke-2"
         />
         <rect
           y="13"
           width="30"
           height="4"
           rx="2"
-          className="origin-center -rotate-45 stroke-black stroke-0 transition-all duration-200 md:group-hover:stroke-2"
+          style={stroke}
+          className="origin-center -rotate-45 stroke-0 transition-all duration-200 md:group-hover:stroke-2"
         />
       </svg>
     </button>
