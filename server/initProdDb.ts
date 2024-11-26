b771524a8a09f26015deab3ecde94e24a40e0d1d
Qodesmith <theqodesmith@gmail.com>
2024-11-26 11:30:35 -0500

    Simplify initializing the prod db

--- server/initProdDb.ts
+++ server/initProdDb.ts
@@ -7,20 +7,10 @@ import {Video} from '@qodestack/dl-yt-playlist'
 import {$} from 'bun'
 import {getDatabase} from './sqlite/db'
 import {beatsTable} from './sqlite/schema'
-import {getEnvVar} from './getEnvVar'
 
-const configPath = `/app/sqlite/drizzle.config.ts`
-const SQLITE_DB_NAME = getEnvVar('SQLITE_DB_NAME')
-const dbPath = `/app/sqlite/${SQLITE_DB_NAME}`
-
-if (Bun.file(dbPath).size !== 0) {
-  throw new Error(`Refusing to proceed. ${SQLITE_DB_NAME} already exists.`)
-}
-
-const metadata: Video[] = await Bun.file('/beats/metadata.json').json()
-
-await $`bunx drizzle-kit push --config="${configPath}"`
+await $`bunx drizzle-kit push --config="/app/sqlite/drizzle.config.ts"`
 
 const db = getDatabase()
+const metadata: Video[] = await Bun.file('/beats/metadata.json').json()
 
 await db.insert(beatsTable).values(metadata)
