24fb42bbca0898a19b971cbf3bad5b7e90ee0fb8
Qodesmith <theqodesmith@gmail.com>
2024-11-26 09:18:16 -0500

    DB url MUST be prefixed with `file:` to avoid path construction errors

--- server/sqlite/drizzle.config.ts
+++ server/sqlite/drizzle.config.ts
@@ -1,16 +1,18 @@
-import dotenv from 'dotenv'
+import path from 'node:path'
+
 import {defineConfig} from 'drizzle-kit'
 
-dotenv.config({path: '../../.env'})
-const {SQLITE_DB_NAME} = process.env
+import {getEnvVar} from '../getEnvVar'
 
-if (!SQLITE_DB_NAME) {
-  throw new Error('No value for SQLITE_DB_NAME env var found.')
-}
+const SQLITE_DB_NAME = getEnvVar('SQLITE_DB_NAME')
 
+/**
+ * DO NOT use `import.meta` in this file. The underlying Drizzle code that
+ * consumes this config is cjs and doesn't work with `import.meta`.
+ */
 export default defineConfig({
-  out: './drizzle',
-  schema: './schema.ts',
+  out: path.resolve(__dirname, './drizzle'),
+  schema: path.resolve(__dirname, './schema.ts'),
   dialect: 'sqlite',
-  dbCredentials: {url: SQLITE_DB_NAME!},
+  dbCredentials: {url: `file:${path.resolve(__dirname, SQLITE_DB_NAME)}`},
 })
