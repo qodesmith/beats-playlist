d3508f4470fa356d52656d5df637410d99a299c0
Qodesmith <theqodesmith@gmail.com>
2024-11-26 11:30:06 -0500

    Use the correct paths for each environment

--- server/sqlite/db.ts
+++ server/sqlite/db.ts
@@ -13,7 +13,10 @@ let db: BunSQLiteDatabase<typeof schema> & {$client: Database}
 export function getDatabase() {
   if (!db) {
     const SQLITE_DB_NAME = getEnvVar('SQLITE_DB_NAME')
-    const dbPath = path.resolve(import.meta.dirname, SQLITE_DB_NAME)
+    const isProd = process.env.NODE_ENV === 'production'
+    const dbPath = isProd
+      ? `/beats/${SQLITE_DB_NAME}`
+      : path.resolve(import.meta.dirname, SQLITE_DB_NAME)
     const sqlite = new Database(dbPath, {create: true})
     db = drizzle({client: sqlite, schema})
   }
--- server/sqlite/drizzle.config.ts
+++ server/sqlite/drizzle.config.ts
@@ -4,15 +4,20 @@ import {defineConfig} from 'drizzle-kit'
 
 import {getEnvVar} from '../getEnvVar'
 
-const SQLITE_DB_NAME = getEnvVar('SQLITE_DB_NAME')
-
 /**
  * DO NOT use `import.meta` in this file. The underlying Drizzle code that
  * consumes this config is cjs and doesn't work with `import.meta`.
  */
+
+const SQLITE_DB_NAME = getEnvVar('SQLITE_DB_NAME')
+const dbPath =
+  process.env.NODE_ENV === 'production'
+    ? `file:/beats/${SQLITE_DB_NAME}`
+    : `file:${path.resolve(__dirname, SQLITE_DB_NAME)}`
+
 export default defineConfig({
   out: path.resolve(__dirname, './drizzle'),
   schema: path.resolve(__dirname, './schema.ts'),
   dialect: 'sqlite',
-  dbCredentials: {url: `file:${path.resolve(__dirname, SQLITE_DB_NAME)}`},
+  dbCredentials: {url: dbPath},
 })
