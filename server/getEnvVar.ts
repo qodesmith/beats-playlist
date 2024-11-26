88024fa05ce2823373f36f85077cd1beb6c38ea5
Qodesmith <theqodesmith@gmail.com>
2024-11-26 09:17:38 -0500

    Added util fxn to get env var

---
+++ server/getEnvVar.ts
@@ -0,0 +1,9 @@
+export function getEnvVar(key: string): string {
+  const value = process.env[key]
+
+  if (value === undefined) {
+    throw new Error(`No value for ${key} env var found.`)
+  }
+
+  return value
+}
--- server/sqlite/db.ts
+++ server/sqlite/db.ts
@@ -6,15 +6,14 @@ import {Database} from 'bun:sqlite'
 import {drizzle} from 'drizzle-orm/bun-sqlite'
 
 import * as schema from './schema'
+import {getEnvVar} from '../getEnvVar'
 
 let db: BunSQLiteDatabase<typeof schema> & {$client: Database}
 
 export function getDatabase() {
   if (!db) {
-    const dbPath = path.resolve(
-      import.meta.dirname,
-      process.env.SQLITE_DB_NAME!
-    )
+    const SQLITE_DB_NAME = getEnvVar('SQLITE_DB_NAME')
+    const dbPath = path.resolve(import.meta.dirname, SQLITE_DB_NAME)
     const sqlite = new Database(dbPath, {create: true})
     db = drizzle({client: sqlite, schema})
   }
