f51a93dee00e11f9857f6bcc40d258bf1ce0f093
Qodesmith <theqodesmith@gmail.com>
2024-12-13 14:14:25 -0500

    Added middelware for routes exclusive to the cron job

---
+++ server/cronOnlyMiddleware.ts
@@ -0,0 +1,54 @@
+import {createMiddleware} from 'hono/factory'
+import {getDatabase} from './sqlite/db'
+import {User, usersTable} from './sqlite/schema'
+import {eq} from 'drizzle-orm'
+
+/**
+ * This middleware requires the cron job's email and password to continue.
+ * The payload will contain email, password, and any other data relevant to the
+ * particular endpoint it's hitting.
+ */
+export const cronOnlyMiddleware = createMiddleware(async (c, next) => {
+  const failedResponse = new Response('Swing and a miss...', {
+    status: 403,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  try {
+    const {email, password, ...data}: {email: string; password: string} =
+      await c.res.json()
+
+    /**
+     * Since the body of a request can only be read once, we store the rest of
+     * the data on the context object for the route handler to retrieve.
+     *
+     * See `type Variables` in `server.ts`.
+     */
+    c.set(c.req.path, data)
+
+    if (!email || email !== Bun.env.EMAIL) {
+      throw new Error('Nope')
+    }
+
+    // TODO - manually add this user to the database.
+    const [user] = await getDatabase()
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.email, email))
+      .limit(1)
+
+    // `user` is potentially undefined even though TS doesn't recognize that.
+    const hash = user?.password ?? ''
+    const verify = Bun.password.verifySync(hash, password)
+
+    if (verify) {
+      await next()
+    } else {
+      c.res = failedResponse
+    }
+  } catch {
+    c.res = failedResponse
+  }
+})
