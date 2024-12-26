97a8373e06b203c8747bb6d2674f615aa7204ea9
Qodesmith <theqodesmith@gmail.com>
2024-12-26 16:06:14 -0500

    Adding the ability to get a fresh DB for testing

--- server/sqlite/db.ts
+++ server/sqlite/db.ts
@@ -10,16 +10,47 @@ import {getEnvVar} from '../getEnvVar'
 
 let db: BunSQLiteDatabase<typeof schema> & {$client: Database}
 
-export function getDatabase() {
+export function getDatabase({fresh = false}: {fresh?: boolean} = {}) {
+  const isProd = process.env.NODE_ENV === 'production'
+  const isTest = process.env.NODE_ENV === 'test'
+
+  if (fresh && isTest) {
+    db?.$client.close()
+    db = drizzle({client: getTestDb(), schema})
+
+    return db
+  }
+
   if (!db) {
     const SQLITE_DB_NAME = getEnvVar('SQLITE_DB_NAME')
-    const isProd = process.env.NODE_ENV === 'production'
     const dbPath = isProd
       ? `/beats/${SQLITE_DB_NAME}`
       : path.resolve(import.meta.dirname, SQLITE_DB_NAME)
-    const sqlite = new Database(dbPath, {create: true})
+    const sqlite = isTest ? getTestDb() : new Database(dbPath)
+
     db = drizzle({client: sqlite, schema})
   }
 
   return db
+}
+
+function getTestDb() {
+  if (Bun.env.NODE_ENV !== 'test') {
+    throw new Error('This function is only for use in tests')
+  }
+
+  const SQLITE_DB_NAME = getEnvVar('SQLITE_DB_NAME')
+  const dbPath = path.resolve(import.meta.dirname, SQLITE_DB_NAME)
+
+  /**
+   * https://bun.sh/docs/api/sqlite#serialize
+   *
+   * When testing, clone the existing database into memory. This enables
+   * us to add and remove data in tests without worry.
+   */
+  const existingDbOnDisk = new Database(dbPath)
+  const contents = existingDbOnDisk.serialize()
+
+  // @ts-expect-error this is how the docs say to do it.
+  return Database.deserialize(contents)
 }
