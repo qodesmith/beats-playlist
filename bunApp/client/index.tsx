6cbc366afa08bde12d917838e9946b7c7df5f3dd
Qodesmith <theqodesmith@gmail.com>
2025-05-10 10:05:52 -0400

    Tanstack Router - engage!

--- bunApp/client/index.tsx
+++ bunApp/client/index.tsx
@@ -4,14 +4,37 @@
  *
  * It is included in `src/index.html`.
  */
+/**
+ * This file is the entry point for the React app, it sets up the root
+ * element and renders the App component to the DOM.
+ *
+ * It is included in `src/index.html`.
+ */
 
+import {createRouter, RouterProvider} from '@tanstack/react-router'
+import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 
-import {App} from './App'
+import {routeTree} from './routeTree.gen'
+
+// Create a new router instance
+const router = createRouter({routeTree})
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 function start() {
   const root = createRoot(document.getElementById('root')!)
-  root.render(<App />)
+
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  )
 }
 
 if (document.readyState === 'loading') {
